15f98ec2628aefc47c9eb5aac25680234d5c6468
Walter Blacke <theoriginalsine@gmail.com>
2018-11-23 14:18:52 -0500

    Added test code

---
+++ Test/src/TestPoolPath.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using NUnit.Framework;
+
+//using SINeDUSTRIES.RegularExpressions;
+
+namespace SINeDUSTRIES.Collections.Test
+{
+  /// <summary>
+  /// <see cref="SINeDUSTRIES.Collections.TreeDirectedRooted{TVertex, TEdge}"/>;
+  /// </summary>
+  [TestFixture]
+  public class TestPoolPath
+  {
+    private IPool<String, StringWrapper> _pool;
+    private StringWrapper _foo;
+    private StringWrapper _bar;
+    private StringWrapper _baz;
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+      _pool = new PoolTest
+        (
+          new StringWrapper("root"),
+          "",
+          '/',
+          EqualityComparer<String>.Default,
+          EqualityComparer<String>.Default // with local equality comparer,
+        );
+
+      _foo = new StringWrapper("foo");
+      _bar = new StringWrapper("bar");
+      _baz = new StringWrapper("baz");
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+      _pool.Clear();
+    }
+
+    [Test]
+    public void TestAddKeyGet()
+    {
+      _pool.Add("/foo", _foo);
+      _pool.Add("/foo/bar", _bar);
+
+      Assert.AreEqual(_pool.Get("/foo/bar"), _bar);
+    }
+
+    /// <summary>
+    /// Test for adding a <see cref="INodeMutable{TElement}"/> where the parent <see cref="INodeMutable{TElement}"/> does not yet exist;
+    /// </summary>
+    /// <remarks>
+    /// implicit parents are an implementation detail that are not publicly exposed; 
+    /// setting a breakpoint here is required;
+    /// </remarks>
+    [Test]
+    public void TestAddKeyImplicit()
+    {
+      _pool.Add("/foo/bar", _bar); // assume add parent implicit "//foo"
+
+      Assert.False(_pool.TryGet("/foo", out StringWrapper parentImplicit)); // assert did not get parent implicit;
+      Assert.AreEqual(parentImplicit, default(StringWrapper)); // assert parent implicit is default
+    }
+
+    /// <summary>
+    /// Tests removal;
+    /// </summary>
+    [Test]
+    public void TestRemove()
+    {
+      _pool.Add("/foo", _foo); // assume add parent
+      _pool.Add("/foo/bar", _bar); // assume add child
+
+      Assert.True(_pool.Remove(_bar)); // assert child removed "//foo/bar"
+      Assert.False(_pool.TryGet("/foo/bar", out StringWrapper element)); // assert child removed "//foo/bar"
+    }
+
+    /// <summary>
+    /// Tests removal of empty branches;
+    /// </summary>
+    /// <remarks>
+    /// implicit parents are an implementation detail that are not publicly exposed; 
+    /// setting a breakpoint here is required;
+    /// </remarks>
+    [Test]
+    public void TestRemoveImplicit()
+    {
+      _pool.Add("/foo/bar", _bar); // assume add parent implcitly "//foo"
+
+      // assertions
+      Assert.True(_pool.Remove(_bar)); // assert remove child  "//foo/bar""
+      Assert.False(_pool.TryGet("/foo", out StringWrapper element)); // assert removed parent implicitly
+    }
+
+    /// <summary>
+    /// <see cref="UtilHierarchy.DesendantsGetPreOrder{T}(IHierarchyChildManyEnumerable{T}, T)"/>;
+    /// </summary>
+    [Test]
+    public void TestDescendantsGet()
+    {
+      // setup
+      _pool.Add("/foo", _foo);
+      _pool.Add("/foo/bar", _bar);
+      _pool.Add("/foo/baz", _baz);
+      IEnumerable<String> children = _pool.DesendantsGetPreOrder(""); // get children
+
+      // assertions
+      Assert.True(children.Contains("/foo"));
+      Assert.True(children.Contains("/foo/bar"));
+      Assert.True(children.Contains("/foo/baz"));
+      Assert.AreEqual(3, children.Count());
+      Assert.AreEqual(children.First(), "/foo");
+      Assert.AreEqual(children.Last(), "/foo/baz");
+    }
+
+    //[Test]
+    //public void TestRegexGet()
+    //{
+    //  // setup
+    //  _pool.Add("//foo", _foo);
+    //  _pool.Add("//foo/bar", _bar);
+    //  _pool.Add("//foo/baz", _baz);
+    //  IEnumerable<String> matches = _pool.IDs.Matches("//foo/.*");
+
+    //  // assertions
+    //  Assert.AreEqual(2, matches.Count());
+    //  Assert.AreEqual(matches.First(), "//foo/bar");
+    //  Assert.AreEqual(matches.Last(), "//foo/baz");
+    //}
+  }
+
+  public class PoolTest : APoolPath<StringWrapper>
+  {
+    protected override String idRelativeChildGet(StringWrapper child)
+    => child.ToString();
+
+    public PoolTest(StringWrapper root, String rootID, Char seperator, IEqualityComparer<String> eqCompGlobal, IEqualityComparer<String> eqCompLocal)
+      : base(root, rootID, seperator, eqCompGlobal, eqCompLocal)
+    {
+    }
+  }
+
+  public class StringWrapper
+  {
+    public String String { get; set; }
+
+    override public String ToString()
+    => String;
+
+    public StringWrapper(String value)
+    {
+      String = value;
+    }
+  }
+}
---
+++ Test/src/TestTreeDirectedRooted.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using QuickGraph;
+
+using NUnit.Framework;
+
+namespace SINeDUSTRIES.Collections.Test
+{
+  /// <summary>
+  /// <see cref="SINeDUSTRIES.Collections.TreeDirectedRooted{TVertex, TEdge}"/>;
+  /// </summary>
+  [TestFixture]
+  public class TestTreeDirectedRooted
+  {
+    #region Fields
+
+    private ITreeDirectedRooted<TestVertex, Edge<TestVertex>> _tree;
+    private TestVertex _childFoo = new TestVertex("foo");
+    private TestVertex _childBar = new TestVertex("bar");
+    private TestVertex _childBaz = new TestVertex("baz");
+
+    #endregion
+
+    // setup
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+      _tree = new TreeDirectedRooted<TestVertex, Edge<TestVertex>>(
+        new TestVertex("root"),
+        EqualityComparer<TestVertex>.Default, // EqCo, global, reference
+        new EqualityComparerVertexLocal() // EqCo, local, values
+      );
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+      _tree.Clear();
+    }
+
+    // tests
+
+    [Test]
+    public void TestRoot()
+    {
+      Assert.AreEqual(_tree.Root.name, "root");
+    }
+
+    [Test]
+    public void TestContainsVertex()
+    {
+      _tree.AddEdge(new Edge<TestVertex>(_tree.Root, _childFoo)); // child
+
+      // using global equality of vertex
+      Assert.True(_tree.ContainsVertex(_childFoo));
+    }
+
+    /// <summary>
+    /// Tests local and global equality;
+    /// </summary>
+    [Test]
+    public void TestEqualityTypes()
+    {
+      _tree.AddEdge(new Edge<TestVertex>(_tree.Root, _childFoo)); // child
+
+      TestVertex vChildQuery = new TestVertex("foo"); // create query
+
+      // local equality on getting edges
+      Assert.True(
+        _tree.TryGetEdge(_tree.Root, vChildQuery, out Edge<TestVertex> edge) && // got a child with the query
+        edge.Target == _childFoo // the object gotten with the query is 
+      );
+
+      Assert.False(_tree.ContainsVertex(vChildQuery)); // global equality on query
+    }
+
+    /// <summary>
+    /// <see cref="IHierarchyChildManyEnumerable{TElement}"/>;
+    /// </summary>
+    [Test]
+    public void TestChildGetMany()
+    {
+      // add children
+      _tree.AddEdge(new Edge<TestVertex>(_tree.Root, _childFoo));
+      _tree.AddEdge(new Edge<TestVertex>(_tree.Root, _childBar)); 
+      _tree.AddEdge(new Edge<TestVertex>(_tree.Root, _childBaz));
+
+      Assert.True(_tree.ChildGetManyTry(_tree.Root, out IEnumerable<TestVertex> children)); // get children
+
+      // contains children
+      Assert.True(children.Contains(_childFoo));
+      Assert.True(children.Contains(_childBar));
+      Assert.True(children.Contains(_childBaz));
+    }
+
+    /// <summary>
+    /// <see cref="UtilHierarchy.DesendantsGetPreOrder{T}(IHierarchyChildManyEnumerable{T}, T)"/>;
+    /// </summary>
+    [Test]
+    public void TestDescendantsGet()
+    {
+      // setup
+      _tree.AddEdge(new Edge<TestVertex>(_tree.Root, _childFoo));
+      _tree.AddEdge(new Edge<TestVertex>(_childFoo, _childBar));
+      _tree.AddEdge(new Edge<TestVertex>(_childFoo, _childBaz));
+
+      // assert get
+      IEnumerable<TestVertex> children = _tree.DesendantsGetPreOrder(_tree.Root); // get children
+
+      // assert contains children
+      Assert.True(children.Contains(_childFoo));
+      Assert.True(children.Contains(_childBar));
+      Assert.True(children.Contains(_childBaz));
+
+      Assert.AreEqual(3, children.Count());
+
+      // assert order
+      Assert.AreEqual(children.First(), _childFoo);
+      Assert.AreEqual(children.Last(), _childBaz);
+    }
+  }
+
+  // type definitions
+
+  public class TestVertex
+  {
+    public String name { get; set; } // auto property
+
+    public TestVertex(String name)
+    {
+      this.name = name;
+    }
+  }
+
+  /// <summary>
+  /// <see cref="EqualityComparer{T}"/> for <see cref="TestVertex"/> which only teests on <see cref="TestVertex.name"/>;
+  /// </summary>
+  public class EqualityComparerVertexLocal : EqualityComparer<TestVertex>
+  {
+    /// <summary>
+    /// Implements <see cref="IEqualityComparer{T}.Equals(T, T)"/>;
+    /// </summary>
+    override public Boolean Equals(TestVertex x, TestVertex y)
+    => x.name == y.name;
+
+    /// <summary>
+    /// Implements <see cref="IEqualityComparer{T}.GetHashCode(T)"/>;
+    /// </summary>
+    override public Int32 GetHashCode(TestVertex obj)
+    => obj.name.GetHashCode();
+  }
+}
